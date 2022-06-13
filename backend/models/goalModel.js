58c642508763094d4fbd9b56663729f305f5e628
Lewis Shum <lewishum96@gmail.com>
2022-06-13 22:39:04 +0800

    Create user login information. Introduce JWT authentication and authorization.

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const goalSchema = mongoose.Schema(
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
         text: {
             type: String,
             required: [true, 'Please add a text value']
@@ -8,7 +13,7 @@ const goalSchema = mongoose.Schema(
     },
     {
         // Timestamp field will be created automatically
-        timestamps: true,
+        timestamps: true
     }
 );
 
